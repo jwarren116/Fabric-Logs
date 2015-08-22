b82d2bb6ea606562d6b6b6a1cf9197a972143866
James Warren <jwarren116@gmail.com>
2015-08-22 11:46:50 -0700

    Add commit and push commands to fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,3 +4,11 @@ from fabric.api import run, local, get
 def fetch_logs():
     logs_dir = "~/logs/jwarren.co/http/*.log"
     get(remote_path=logs_dir)
+
+
+def commit():
+    local("git add -p && git commit")
+
+
+def push():
+    local("git push")
